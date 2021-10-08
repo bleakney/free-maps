2a3abd736e30690b771000c122d7adc6527fccb1
unknown <fizz.zehra14@gmail.com>
2021-10-08 15:04:42 -0500

    creating items schemas

---
+++ server/models/Items.js
@@ -0,0 +1,35 @@
+const { Schema } = require('mongoose');
+const UserSchema = require('./User')
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const itemSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    donator: {
+        type: UserSchema,
+        required: true
+
+    }
+});
+
+module.exports = itemSchema;
