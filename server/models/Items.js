d6e8ffc6042e7ae1e271c926035104beffd39ae2
unknown <fizz.zehra14@gmail.com>
2021-10-08 15:14:56 -0500

    setting donation attr that user is making into an array

--- server/models/Items.js
+++ server/models/Items.js
@@ -1,6 +1,6 @@
 const { Schema } = require('mongoose');
 const UserSchema = require('./User')
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `Donation` array in User.js
 const itemSchema = new Schema({
     title: {
         type: String,
