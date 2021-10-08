2ac79b45b7dc9c47a703380caf1081b155da051e
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-08 16:00:57 -0500

    modify map component to fill viewport, source custom mapbox style

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL from 'react-map-gl'; 
 
 function Map() {
   const [viewport, setViewport] = useState({
-    width: 800,
-    height: 400,
-    latitude: 30.2672,
-    longitude: -97.7431,
-    zoom: 8,
+    latitude: 30.266013,
+    longitude: -97.746211,
+    zoom: 12
   });
 
   return (
-    <ReactMapGL
+      <div>
+    <ReactMapGL className="map"
       {...viewport}
+      width="100vw"
+      height="100vh"
+      mapStyle="mapbox://styles/bleakney/ckuisjewn1ymi17qqybxmk3e8"
       mapboxApiAccessToken="pk.eyJ1IjoiYmxlYWtuZXkiLCJhIjoiY2t1ZzJjb2RiMjBjMTJvbzhubnNqdWlncSJ9.NC3BTCx2RMAohqDdk2BW9A"
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
+    </div>
   );
 }
 
