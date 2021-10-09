02a1e28ae733c061262511bfd0d515debeef31b5
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-09 11:47:49 -0500

    set up basic html of search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 // import dependencies
-import React, { useEffect } from 'react';
-import WebFont from 'webfontloader';
-import './index.scss';
+// import React, { useEffect } from 'react';
+// import { func } from 'prop-types';
+import React from 'react';
+// import WebFont from 'webfontloader';
+// import './index.scss';
 
 // import components
-import Map from './components/Map';
-import Header from './components/Header';
+// import Map from './components/Map';
+// import Header from './components/Header';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div>
+      < Search />
+    </div>
+  )
+}
 // import { createTheme } from '@mui/material';
 // import { ThemeProvider } from '@emotion/react';
 
@@ -15,23 +26,23 @@ import Header from './components/Header';
 //   }
 // });
 
-function App() {
-  // import google fonts
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: ['Playfair Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900', 'Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900']
-      }
-    })
-  })
-  return (
-    // <ThemeProvider theme={darkTheme}>
-    <div className="app-container">
-       <Header className="header" />
-      <Map />
-    </div>
-    // </ThemeProvider>
-  );
-}
+// function App() {
+//   // import google fonts
+//   // useEffect(() => {
+//   //   WebFont.load({
+//   //     google: {
+//   //       families: ['Playfair Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900', 'Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900']
+//   //     }
+//   //   })
+//   // })
+//   return (
+//     // <ThemeProvider theme={darkTheme}>
+//     <div className="app-container">
+//        <Header className="header" />
+//       <Map />
+//     </div>
+//     // </ThemeProvider>
+//   );
+// }
 
 export default App;
--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Search() {
-
+  return (
+    <form action="/" method="get">
+      <label class="search-bar">
+        <span className="visually-hidden">Search by City/Zipcode</span>
+      </label>
+      <input type="text" id="header-search" placeholder="Search " name="s" />
+      <button type="submit">Search</button>
+    </form>
+  );
 }
 
 export default Search;
