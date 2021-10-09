b787427c0eb9c0e2466d52f6f66cf906772b2391
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-09 15:38:07 -0500

    footer styling and added extra icons

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
   return (
     <div>
+      <FontAwesomeIcon icon={faMapMarkerAlt} size="5x" color="rgb(116, 216,254)" alignmentBaseline="central"> </FontAwesomeIcon> 
       <h1>Free Maps</h1>
       <div className="box">
         <FontAwesomeIcon icon={faSearch} size="2x"></FontAwesomeIcon>
