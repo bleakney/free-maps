0db03ba40b0a54912045a112a6e70b65ddc39735
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-09 12:44:34 -0500

    styling off search bar and page

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
   return (
-   <div>
-       <h1>Free Maps</h1>
-       <div className="box">
-           <input type="text" name=""></input>
-           <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
-       </div>
-   </div>
+    <div>
+      <h1>Free Maps</h1>
+      <div className="box">
+        <FontAwesomeIcon icon={faSearch} size="2x"></FontAwesomeIcon>
+        <input type="text" name=""></input>
+      </div>
+    </div>
   );
 }
 
