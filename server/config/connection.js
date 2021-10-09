2b6d9aa4b90b610080130e7e38e493a84be48feb
unknown <fizz.zehra14@gmail.com>
2021-10-09 01:00:37 -0500

    creating authentications utils/auth

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/freemaps', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/free-maps', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
