45f30b8065d3ffef527a8be0ca78602afd206c2a
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-09 17:50:17 -0500

    export ADD_USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,4 +10,16 @@ mutation login($email: String!, $password: String!) {
         }
     }
 }
-`
+`;
+
+export const ADD_USER = gql `
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
