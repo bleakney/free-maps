b14c47f79e87e76dce69694b33ccdc2ceb643726
unknown <fizz.zehra14@gmail.com>
2021-10-09 01:06:08 -0500

    creating authentications utils/auth

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
     //uncomment it once the client side is done
-//  app.use(express.static(path.join(__dirname, '../client/build')));
+ app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
