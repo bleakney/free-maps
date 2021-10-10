94545c09e9c61fb2a49785c23f91ad504360adcb
unknown <fizz.zehra14@gmail.com>
2021-10-10 12:34:26 -0500

    creating mutations

--- server/server.js
+++ server/server.js
@@ -59,9 +59,10 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+let server=null;
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
+   server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
     context: authMiddleware 
@@ -84,7 +85,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3001, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
