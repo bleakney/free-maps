ebc996e773449a11d408485006d5ef5390b763a7
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-11 14:30:30 -0500

    import MapPin component to Map component and create test items array to test functionality

--- client/src/components/MapPins/index.js
+++ client/src/components/MapPins/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Marker } from 'react-map-gl';
+import RoomIcon from '@mui/icons-material/Room';
 
 const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
@@ -16,7 +17,9 @@ function MapPins(props) {
         key={`marker-${index}`}
         longitude={item.longitude}
         latitude={item.latitude}>
-            <svg
+
+            <RoomIcon sx={{color: "#000"}}></RoomIcon>
+            {/* <svg
             height={SIZE}
             viewbox="0 0 24 24"
             style={{
@@ -28,7 +31,7 @@ function MapPins(props) {
             // onClick={() => onClick(city)}
             >
                 <path d={ICON} />
-            </svg>
+            </svg> */}
         </Marker>
     ));
 }
