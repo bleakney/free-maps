3534fa5d9734cc847f40595d24a3ffa74d445a39
James Bowen (J.D.) <j.bowen1821@gmail.com>
2021-10-11 01:35:31 -0500

    add user-controllers

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 
 const { authMiddleware } = require('../../utils/auth');
 
+router.route('/').post(createUser).put(authMiddleware, saveBook);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
 module.exports = router;
