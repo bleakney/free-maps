0a0180e8629a09023db795eb4c2af9767ffd4544
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-11 18:38:35 -0500

    styling of social icons

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,9 +8,9 @@ function Footer() {
         <button id="donate-button">Donate Here</button>
       </div>
       <div className="icon-logos">
-        <SocialIcon url="https://github.com/bleakney/free-maps" />
-        <SocialIcon url="https://twitter.com/" />
-        <SocialIcon url="https://linkedin.com/" />
+        <SocialIcon url="https://github.com/bleakney/free-maps" style={{ height: 35, width: 35 }} />
+        <SocialIcon url="https://twitter.com/" style={{ height: 35, width: 35 }} />
+        <SocialIcon url="https://linkedin.com/"  style={{ height: 35, width: 35 }} />
       </div>
     </div>
   );
