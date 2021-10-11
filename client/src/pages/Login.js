a70b4f6ae19ac0eefb2b40f70df40bf408bf8994
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-11 11:40:37 -0500

    login page updated

---
+++ client/src/pages/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <main className='flex-row justify-center mb-4'>
+      <div className='col-12 col-md-6'>
+        <div className='card'>
+          <h4 className='card-header'>Login</h4>
+          <div className='card-body'>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className='form-input'
+                placeholder='Your email'
+                name='email'
+                type='email'
+                id='email'
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className='btn d-block w-100' type='submit'>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
