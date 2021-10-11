0f21e0141a1e7cff1f7136bde5526cebd5b64747
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-10 20:04:41 -0500

    items inserted into queries.js client

---
+++ client/utils/queries.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ITEMS = gql`
+  query items($username: String) {
+    items(username: $username) {
+      _id
+      title
+      description
+      image
+      status
+      quantity
+    }
+  }
+`;
--- server/routes/api/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-const { authMiddleware } = require('../../utils/auth');
-
-module.exports = router;
