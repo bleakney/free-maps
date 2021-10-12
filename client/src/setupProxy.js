d451ee57b12318f431e5868ec07f41d7ab6b0d14
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-12 17:10:51 -0500

    uncomment code

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,11 @@
-// const createProxyMiddleware = require('http-proxy-middleware');
+const createProxyMiddleware = require('http-proxy-middleware');
 
-
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'https://localhost:3000',
-//       changeOrigin: true
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://localhost:3000',
+      changeOrigin: true
+    })
+  );
+};
