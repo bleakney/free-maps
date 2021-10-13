ee42977d93523a5d6e58a13205e389b861e7eb99
unknown <fizz.zehra14@gmail.com>
2021-10-12 22:39:49 -0500

    creating server worker

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -4,9 +4,9 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./",
+    "/",
     'index.html',
-    'js/index.js',
+    'src/index.js',
     'css/styles.css',
     "icons/icon-192x192.png", 
     "icons/icon-72x72.png",
