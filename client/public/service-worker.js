ac7b5d482eab76879b564529b1cd577959cedbf4
unknown <fizz.zehra14@gmail.com>
2021-10-12 22:44:41 -0500

    creating server worker

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,5 +1,5 @@
 //define gloabl constants
-const APP_PREFIX = 'FreeMaps-';     
+const APP_PREFIX = 'FreeMaps-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -7,23 +7,16 @@ const FILES_TO_CACHE = [
     "./",
     'index.html',
     '../src/index.js',
-    "icons/icon-192x192.png", 
-    "icons/icon-72x72.png",
-    "icons/icon-96x96.png",
-    "icons/icon-128x128.png",
-    "icons/icon-144x144.png",
-    "icons/icon-152x152.png",
-    "icons/icon-192x192.png",
-    "icons/icon-384x384.png",
-    "icons/icon-512x512.png",
-    'manifest.json' 
+    'logo192.png',
+    'logo152.png',
+    'manifest.json'
 ];
 
 self.addEventListener('install', function (e) {
     //perform install steps
     e.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then(function (cache) {
                 return cache.addAll(FILES_TO_CACHE);
             })
     );
@@ -31,23 +24,23 @@ self.addEventListener('install', function (e) {
 });
 
 //activate and instructions to manage cache
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     e.waitUntil(
-      caches.keys().then(function(keyList) {
-        let cacheKeeplist = keyList.filter(function(key) {
-          return key.indexOf(APP_PREFIX);
-        });
-        cacheKeeplist.push(CACHE_NAME);
-  
-        return Promise.all(
-          keyList.map(function(key, i) {
-            if (cacheKeeplist.indexOf(key) === -1) {
-              console.log('deleting cache : ' + keyList[i]);
-              return caches.delete(keyList[i]);
-            }
-          })
-        );
-      })
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
     );
 });
 
@@ -55,19 +48,19 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-      caches.match(e.request)
-        .then(function (request) {
-            // if cache is available, respond with cache
-            if (request) { 
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-        }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
+        caches.match(e.request)
+            .then(function (request) {
+                // if cache is available, respond with cache
+                if (request) {
+                    console.log('responding with cache : ' + e.request.url)
+                    return request
+                } else {       // if there are no cache, try fetching request
+                    console.log('file is not cached, fetching : ' + e.request.url)
+                    return fetch(e.request)
+                }
+
+                // You can omit if/else for console.log & put one line below like this too.
+                // return request || fetch(e.request)
+            })
     );
 });
