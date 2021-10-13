a9e39387dfa82b5e6a4cfb666959f9f628965288
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-13 14:38:23 -0500

    pull item info from server and display in menu

--- client/src/components/ItemsList/index.js
+++ client/src/components/ItemsList/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React from "react";
 
-function ItemsList () {
+function ItemsList({ items }) {
+  if (!items.length) {
+    return <div>Nothing up for grabs in your area</div>;
+  }
 
+  return (
+    <div className="itemListContainer">
+      {items &&
+        items.map((item) => (
+          <div key={item._id} className="itemContainer">
+            <h4 className="itemTitle">{item.title}</h4>
+            <div>
+              <p>{item.description}</p>
+              <p>{item.quantity}</p>
+            </div>
+            <div className="itemFooterContainer">
+              <p className="itemFooterText">{item.createdAt}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
+
+export default ItemsList;
--- client/src/components/MenuDrawer/index.js
+++ client/src/components/MenuDrawer/index.js
@@ -1,80 +1,88 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import IconButton from '@mui/material/IconButton'
-import Divider from '@mui/material/Divider';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import IconButton from "@mui/material/IconButton";
+import Divider from "@mui/material/Divider";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { useQuery } from "@apollo/client";
+import { QUERY_ITEMS } from "../../utils/queries";
+import ItemsList from '../ItemsList';
 
 const drawerWidth = "30vw";
 const tabStyles = {
-    color: "rgb(239, 235, 239)",
-    "& span": {
-        color: "rgb(191, 171, 171)!important"
-    }
-}
-
+  color: "rgb(239, 235, 239)",
+  "& span": {
+    color: "rgb(191, 171, 171)!important",
+  },
+};
 
 function MenuDrawer(props) {
-    // import useState function to toggle drawer visibility
-    const {
-        openDrawer,
-        setDrawerOpen
-    } = props;
-    const handleDrawerClose = () => setDrawerOpen(false);
+  // import useState function to toggle drawer visibility
+  const { openDrawer, setDrawerOpen } = props;
+  const handleDrawerClose = () => setDrawerOpen(false);
+  // toggle Tabs
+  const [tabValue, setTabValue] = useState("1");
+  const handleChange = (event, newValue) => setTabValue(newValue);
 
-    // toggle Tabs
-    const [tabValue, setTabValue] = useState('1');
-    const handleChange = (event, newValue) => setTabValue(newValue);
+  // query items
+  const { loading, data } = useQuery(QUERY_ITEMS);
+  const items = data?.items || []
+  console.log(items);
 
-    return (
-        <div className="drawer-container">
-    <Drawer 
-    sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        '& .MuiDrawer-paper': {
+  return (
+    <div className="drawer-container">
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: 'border-box',
-            bgcolor: 'rgb(194, 228, 255)'
-        },
-    }}
-    variant="persistent"
-    anchor="left"
-    open={openDrawer}
-    >
+            boxSizing: "border-box",
+            bgcolor: "rgb(194, 228, 255)",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={openDrawer}
+      >
         <div className="drawer-header">
-            <IconButton onClick={handleDrawerClose}>
-                <ChevronLeftIcon />
-            </IconButton>
-       </div>
-        <Box sx={{ width: '100%'}}>
-      <TabContext value={tabValue}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <div className="tablist-container">
-          <TabList onChange={handleChange} aria-label="Pin descriptions" >
-            <Tab label="View All" value="1" sx={tabStyles} />
-            <Tab label="Your Pins" value="2" sx={tabStyles} />
-            <Tab label="Saved Pins" value="3" sx={tabStyles} />
-          </TabList>
-          
-          </div>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Box sx={{ width: "100%" }}>
+          <TabContext value={tabValue}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <div className="tablist-container">
+                <TabList onChange={handleChange} aria-label="Pin descriptions">
+                  <Tab label="View All" value="1" sx={tabStyles} />
+                  <Tab label="Your Pins" value="2" sx={tabStyles} />
+                  <Tab label="Saved Pins" value="3" sx={tabStyles} />
+                </TabList>
+              </div>
+            </Box>
+            {/* maybe use map function here */}
+            <TabPanel value="1">
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <ItemsList 
+                    items={items}
+                    />
+                )}
+            </TabPanel>
+            <TabPanel value="2">Item Two</TabPanel>
+            <TabPanel value="3">Item Three</TabPanel>
+          </TabContext>
         </Box>
-        {/* maybe use map function here */}
-        <TabPanel value="1">Item One</TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
-      </TabContext>
-    </Box>
         <Divider />
-
-    </Drawer>
+      </Drawer>
     </div>
-    )
+  );
 }
 
 export default MenuDrawer;
