16cc708653ce1233755262fef55e097feb5f5ab4
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-14 17:23:45 -0500

    fix bug with website crashing when user is not logged in

--- client/src/components/ItemsList/index.js
+++ client/src/components/ItemsList/index.js
@@ -32,8 +32,9 @@ function ItemsList({ items }) {
   if (!items.length) {
     return <div>Nothing up for grabs in your area</div>;
   }
-  const userData = Auth.getProfile();
-  const username = userData.data.username;
+
+//   const userData = Auth.getProfile();
+//   const username = userData.data.username;
 
 
   // create useState function linked to addItem to automatically refresh list when updated
@@ -44,7 +45,7 @@ function ItemsList({ items }) {
       {items &&
         items.map((item) => (
           <div key={item._id} className="itemContainer">
-              {item.username === username ? (
+              {Auth.loggedIn() && item.username === Auth.getProfile().data.username ? (
                   <>
                   <div className="deleteItemContainer">
                 <h4 className="itemTitle">{item.title}</h4>
